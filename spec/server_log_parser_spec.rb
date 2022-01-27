40b0bc143605ceed0700c3a4abcd32b2123b15d3
Nilay Anand <nilayanand@gmail.com>
2022-01-28 00:44:21 +0530

    Add some unit tests

--- spec/server_log_parser_spec.rb
+++ spec/server_log_parser_spec.rb
@@ -1,7 +1,49 @@
 require 'spec_helper'
-RSpec.describe "ServerLogParser" do
+require File.dirname(__FILE__) + '/../lib/' + 'server_log_parser.rb'
 
-  it "does something useful" do
-    expect(true).to eq(true)
+RSpec.describe "ServerLogParser" do
+  context 'when non existing file given' do
+    before do
+      @parser = ServerLogParser.new('non-existing.log')
+      @parser.parse
+    end
+
+    it 'should fail when non existing file given' do
+      expect(@parser.success?).to eq false
+    end
+
+    it 'has error message' do
+      expect(@parser).to have_attributes(message: "ERROR: 'non-existing.log' do not exists")
+    end
   end
+
+  context 'when existing file given' do
+    before do
+      @parser = ServerLogParser.new('spec/data/server.log')
+      @result = @parser.parse
+    end
+
+    it 'should success' do
+      expect(@parser.success?).to eq true
+    end
+
+    it 'has no error message' do
+      expect(@parser).to have_attributes(message: nil)
+    end
+
+    it 'should have four records' do
+      expect(@result.count).to eq 4
+    end
+
+    it 'should have highest total count on top' do
+      expect(@result[0][:uri]).to eq '/help_page/1'
+    end
+
+    it 'should calculate unique count correctly' do
+      expect(@result[0][:count]).to eq 3
+      expect(@result[0][:unique_count]).to eq 2
+    end
+
+  end
+
 end
